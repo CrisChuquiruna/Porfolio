1ade54ce859d87861b95a2db59d66d3320118fd9
Cristian Chuquiruna <cris.chuquiruna@gmail.com>
2023-08-08 20:11:18 -0300

    Ligero cambio de colores y nuevo boton de link a github en los proyectos

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,12 +14,14 @@ export const Projects = ({ title }) => {
         title={'Cuatro en linea'}
         text={'Desarrollado con Javascript, HTML y CSS. En el contexto de practicar Programación orientada a objetos.'}
         link={'https://crischuquiruna.github.io/Cuatro-en-Linea/'}
+        linkGit={'https://github.com/CrisChuquiruna/Cuatro-en-Linea'}
       />
       <Card
         img={imgPorfolioArtista}
         title={'Porfolio para artista'}
         text={'Desarrollado en React cuenta con una api en node que conecta a Flickr para facilitar la subida de nuevas imagenes.'}
         link={'https://camilacaffieri.netlify.app/'}
+        linkGit={'https://github.com/CrisChuquiruna/Porfolio-Artista'}
       />
     </section>
     </>
