8ecc764efe39d3279aa6359411d49de48c9934ea
Cristian Chuquiruna <cris.chuquiruna@gmail.com>
2025-04-02 11:05:15 -0300

    Update Projects.jsx
    Eliminate deprecated projecets

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -17,20 +17,6 @@ export const Projects = ({ title }) => {
         link={'https://crischuquiruna.github.io/Cuatro-en-Linea/'}
         linkGit={'https://github.com/CrisChuquiruna/Cuatro-en-Linea'}
       />
-      <Card
-        img={imgPorfolioArtista}
-        title={'Porfolio para artista'}
-        text={'Desarrollado en React cuenta con una api en node que conecta a Flickr para facilitar la subida de nuevas imagenes.'}
-        link={'https://camilacaffieri.netlify.app/'}
-        linkGit={'https://github.com/CrisChuquiruna/Porfolio-Artista'}
-      />
-      <Card
-        img={imgMovieRoulette}
-        title={'Movie Roulette'}
-        text={'Front en React y Back con Django, una web para tener tus lista de peliculas pendientes y sortear una al azar de esa lista.'}
-        link={'https://youmovieroulette.netlify.app/'}
-        linkGit={'https://github.com/CrisChuquiruna/MovieRoulette'}
-      />
     </section>
     </>
   )
